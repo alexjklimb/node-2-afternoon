5b94322e2df9ebb2379c159d73e19049963df2e3
alexjklimb <alexjklimb@gmail.com>
2020-06-23 17:14:15 -0800

    project complete

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mc = require("./controllers/messages_controller");
+const app = express();
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
